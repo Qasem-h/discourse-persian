0f5166078a7ab1aaaa232d92020863820bc281c7
Qasem Hajizadeh <gasemhacker@gmail.com>
2015-05-12 13:47:35 +0430

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,16 @@
+# name: Persian-Discourse
+# about: www.discours.ir
+# version: 1.3
+# authors: Qasem Hajizadeh
+
+register_asset "app/assets/javascripts/locales/fa_IR.js.erb"
+register_asset "config/locales/client.fa_IR.yml"
+register_asset "config/locales/server.fa_IR.yml"
+register_asset "plugins/poll/config/locales/client.fa_IR.yml"
+register_asset "plugins/poll/config/locales/ server.fa_IR.yml"
+register_asset "public/403.fa_IR.html"
+register_asset "public/422.fa_IR.html"
+register_asset "public/500.fa_IR.html"
+register_asset "public/503.fa_IR.html"
+register_asset "vendor/gems/discourse_imgur/lib/discourse_imgur/locale/server.fa_IR.yml"
 
